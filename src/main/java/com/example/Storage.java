b043e9a382c9da9fe843e2e5284afdbb572a1b06
Csorba Péter <csosaka21@gmail.com>
2026-03-02 15:31:21 +0100

    Fájlba író osztály (Storage) működik

--- src/main/java/com/example/Storage.java
+++ src/main/java/com/example/Storage.java
@@ -1,7 +1,23 @@
 package com.example;
 
-public class Storage {
-    public static void writeToFile(String content)  {
+import java.io.FileWriter;
+import java.io.IOException;
 
+
+
+public class Storage {
+    public static void writeToFile(String content){
+        try {
+            trywriteToFile(content);
+        } catch (IOException e) {
+            System.err.println("Hiba! A fájl nem írható!");
+            System.err.println(e.getMessage());
+        }   
+    }
+
+    public static void trywriteToFile(String content) throws IOException  {
+        FileWriter fileWriter = new FileWriter("adat.txt");
+        fileWriter.write(content);
+        fileWriter.close();
     }
 }
